bdb2fba89d704043f0d67277549f3ae5ea4e64f3
DigambarPS <74504931+DigambarPS@users.noreply.github.com>
2022-09-06 11:06:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import './paragraph.js';
+
+function App() {
+  return (
+    <>
+    <diV className="App">
+      <h1>Hello world</h1>
+    </diV>
+    
+    <div className="App-header">
+        <h2>This is a simple web page</h2>
+      </div>
+
+    <div className="para">
+      <h3> This is a paragraph</h3>  
+    </div>
+
+    <div className="App-link">
+      <h2><a href="">click here</a></h2>
+    </div>  
+    </>
+  );
+}
+
+export default App;
---
+++ paragraph.js
@@ -0,0 +1,8 @@
+import "./para.css";
+
+function paragraph(){
+    return 
+    <p> This is a paragraph</p>
+    ;
+}
+export default paragraph;
